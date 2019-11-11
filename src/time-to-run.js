c54ea6772b6e594416392aaec72208e0ad16bd28
Dominika Tazikova <52998432+Domi-hub@users.noreply.github.com>
2019-11-11 22:03:47 +0100

    Add google maps to a new component

---
+++ src/time-to-run.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { compose, withProps } from "recompose";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+export default function TimeToRun() {
+    const Component = compose(
+        withProps({
+          googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+          loadingElement: <div style={{ height: `100%` }} />,
+          containerElement: <div style={{ height: `400px` }} />,
+          mapElement: <div style={{ height: `100%` }} />,
+        }),
+        withScriptjs,
+        withGoogleMap
+      )((props) =>
+        <GoogleMap
+            defaultZoom={8}
+            defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        >
+            <Marker position={{ lat: -34.397, lng: 150.644 }} />
+        </GoogleMap>
+    );
+
+    return (
+        <Component />
+    );
+}
